6272e22c50fedd04193dc7504fb954ee20227bab
Eric Lee Santos <eric_lee@MacBook-Pro-de-Eric.local>
2019-08-29 09:29:55 -0300

    Adding arm() and disarm() to lab06.h

--- programs/labs/lab06.h
+++ programs/labs/lab06.h
@@ -10,11 +10,15 @@ class Mixer
         PwmOut motor2;
         PwmOut motor3;
         PwmOut motor4;
+        DigitalOut led1;
+        DigitalOut led2;
 
         float omega1;
         float omega2;
         float omega3;
         float omega4;
+
+        bool armed;
 
         float checarRaiz(float x)
         {
@@ -42,24 +46,48 @@ class Mixer
         }
 
     public:
-        Mixer() : motor1(MOTOR1), motor2(MOTOR2), motor3(MOTOR3), motor4(MOTOR4)
-        {
+        Mixer() : motor1(MOTOR1), motor2(MOTOR2), motor3(MOTOR3), motor4(MOTOR4), led1(LED_RED_R), led2(LED_GREEN_L)
+        {}
 
-        }
         void actuate(float f_t, float tau_phi, float tau_theta, float tau_psi)
         {
-            mixer(f_t, tau_phi, tau_theta, tau_psi);
-            motor1 = motorControl(omega1);
-            motor2 = motorControl(omega2);
-            motor3 = motorControl(omega3);
-            motor4 = motorControl(omega4);
+            if(armed)
+            {
+                mixer(f_t, tau_phi, tau_theta, tau_psi);
+                motor1 = motorControl(omega1);
+                motor2 = motorControl(omega2);
+                motor3 = motorControl(omega3);
+                motor4 = motorControl(omega4);
+            }
         }
+        
+        void arm()
+        {
+            armed=true;
+            led1=true;
+            led2=false;
+        }
+
+        void disarm()
+        {
+            armed=false;
+            led1=false;
+            led2=true;
+            motor1=0.0;
+            motor2=0.0;
+            motor3=0.0;
+            motor4=0.0;
+        }
+
 };
 
 Mixer mixer;
 
 int main()
 {
-    mixer.actuate(0,0,0,-0.001);;
+    mixer.arm();
+    mixer.actuate(0,0.005,0,0);
+    wait(5.0);
+    mixer.disarm();
     return 0;
 }
