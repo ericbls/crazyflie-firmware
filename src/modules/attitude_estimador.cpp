4f054320897bed9fe6052edb5bc7dcf1c9ad5dd2
Eric Lee Santos <eric_lee@MacBook-Pro-de-Eric.local>
2019-09-17 09:33:45 -0300

    Adicionando lab07 de estimador de altitude. Com Pedro Garcia e ERIC lEE.

---
+++ src/modules/attitude_estimador.cpp
@@ -0,0 +1,26 @@
+#include "mbed.h"
+#include "attitude_estimador.h"
+
+//Class constructor
+AttitudeEstimador::AttitudeEstimador():imu(IMU_SDA, IMU_SCL)
+{
+    phi=0;
+    theta=0;
+    psi=0;
+
+    p=0;
+    q=0;
+    r=0;
+}
+
+//Initialize class
+void AttitudeEstimador::init()
+{
+    imu.init();
+}
+
+//Estimate Euler angles (rad) and angular velocities (rad/s)
+void AttitudeEstimador::estimate()
+{
+
+}
---
+++ src/modules/attitude_estimador.h
@@ -0,0 +1,29 @@
+#ifndef atitude_estimador_h
+#define atitude_estimador_h
+
+#include "mbed.h"
+#include "src/utils/pin_names.h"
+#include "src/utils/parameters.h"
+#include "src/drivers/mpu9250.h"
+
+// Attitude estimador class
+class AttitudeEstimador
+{
+    public:
+    //Class constructor
+    AttitudeEstimador();
+    //Initialize class
+    void init();
+    //Estimate Euler angles (rad) and angular velocities (rad/s)
+    void estimate();
+    //Euler angles
+    float phi, theta, psi;
+    //Angular velocities (rad/s)
+    float p, q, r;
+
+    private:
+    //IMU sensor object
+    MPU9250 imu;
+};
+
+#endif
